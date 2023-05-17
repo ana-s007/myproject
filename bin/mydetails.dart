da6437e8113f533ff625be20cb30ecc818e18c33
hp <muhammedanask20@gmail.com>
2023-05-17 13:05:57 +0530

    first commit

---
+++ bin/mydetails.dart
@@ -0,0 +1,24 @@
+void main(){
+  String name = "zoro";
+  int age = 21;
+  String email = "Roronouazoro@gmail.com";
+  int phone = 811182449;
+  double cgpa = 10;
+  String quali  = "swordsman";
+  String loca = "land of wano-kuni";
+
+
+
+  print(name);
+
+  print("My name is $name");
+  print('I am $age year old');
+  print("My email id is $email");
+  print("My phone number is $phone");
+  print("I am a $quali graduate");
+  print("CGPA in graduation is $cgpa");
+  print("I am from $loca");
+
+
+
+}
